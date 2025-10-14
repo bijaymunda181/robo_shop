56d5a2ff55bd1a288fcd885014059470e1f1a480
Bijay <bijay78082@gmail.com>
2025-10-14 12:33:59 +0530

    echo command in project

--- frontend.sh
+++ frontend.sh
@@ -1,24 +1,24 @@
-echo "<<<<<<<< install nginux >>>>>>>>>>>>"
+ echo -e "\e[36m<<<<<<<< install nginux >>>>>>>>>>>\e[0m"
 yum install nginx -y
 
-echo "<<<<<<< copy nginx-roboshop.conf file"
+ echo -e "\e[36m<<<<<<< copy nginx-roboshop.conf file\e[0m"
 cp nginx-roboshop.conf /etc/nginx/default.d/roboshop.conf
 
-echo "<<<<<<<<<<<<<<  remove all inside the /usr/share/nginx/html/* file >>>>>>>>>>>>"
+ echo -e "\e[36m<<<<<<<<<<<<<<  remove all inside the /usr/share/nginx/html/* file >>>>>>>>>>>\e[0m"
 rm -rf /usr/share/nginx/html/*
 
-echo " <<<<<<<<<<<<<<<<<<<<    install unzip command   >>>>>>>>>>>>>>>>>>>"
+ echo -e "\e[36m <<<<<<<<<<<<<<<<<<<<    install unzip command   >>>>>>>>>>>>>>>>>>\e[0m"
 yum install unzip -y
 
-echo "<<<<<<<<<<<<     download the zip file    >>>>>>>>>>>>>>>>"
+ echo -e "\e[36m<<<<<<<<<<<<     download the zip file    >>>>>>>>>>>>>>>\e[0m"
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
 
-echo "<<<<<<<<<<<<<<<<<<<<   go to html directory     >>>>>>>>>>>>>>>>"
+ echo -e "\e[36m<<<<<<<<<<<<<<<<<<<<   go to html directory     >>>>>>>>>>>>>>>\e[0m"
 cd /usr/share/nginx/html
 
-echo " <<<<<<<<<<<<<<<<<<<<<<     unzip frontend.zip file    >>>>>>>>>>>>>>>>>>>>>>>>>>>"
+ echo -e "\e[36m <<<<<<<<<<<<<<<<<<<<<<     unzip frontend.zip file    >>>>>>>>>>>>>>>>>>>>>>>>>>\e[0m"
 unzip /tmp/frontend.zip
 
-echo " <<<<<<<<<<<<<<<<<<<<<<<<     start and enable the nginx service     >>>>>>>>>>>>>>>>>>>>>>>"
+ echo -e "\e[36m <<<<<<<<<<<<<<<<<<<<<<<<     start and enable the nginx service     >>>>>>>>>>>>>>>>>>>>>>\e[0m"
 systemctl enable nginx
 systemctl restart nginx
