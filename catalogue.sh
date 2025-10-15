25f2db70f7a9ec24e15deaf20b2e6a813257357b
Bijay <bijay78082@gmail.com>
2025-10-15 13:30:19 +0530

    echo command in scripts

--- catalogue.sh
+++ catalogue.sh
@@ -1,42 +1,42 @@
-echo ">>>>>>>>>>>>>>>>>>   create catalogue service file   <<<<<<<<<<<<<<<<<"
+echo -e "\e[36m>>>>>>>>>>>>>>>>>>   create catalogue service file   <<<<<<<<<<<<<<<<\e[0m"
 cp catalogue.service /etc/systemd/system/catalogue.service
 
-echo ">>>>>>>>>>>>>>>>>>   create mongodb repo   <<<<<<<<<<<<<<<<<"
+echo -e "\e[36m>>>>>>>>>>>>>>>>>>   create mongodb repo   <<<<<<<<<<<<<<<<\e[0m"
 cp mongo.repo /etc/yum.repos.d/mongo.repo
 
-echo ">>>>>>>>>>>>>>>>>>   installing unzip command   <<<<<<<<<<<<<<<<<"
+echo -e "\e[36m>>>>>>>>>>>>>>>>>>   installing unzip command   <<<<<<<<<<<<<<<<\e[0m"
 yum intsall unzip -y
 
-echo ">>>>>>>>>>>>>>>>>>   install  NodeJS Repos   <<<<<<<<<<<<<<<<<"
+echo -e "\e[36m>>>>>>>>>>>>>>>>>>   install  NodeJS Repos   <<<<<<<<<<<<<<<<\e[0m"
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash
 
-echo ">>>>>>>>>>>>>>>>>>   install NodeJS   <<<<<<<<<<<<<<<<<"
+echo -e "\e[36m>>>>>>>>>>>>>>>>>>   install NodeJS   <<<<<<<<<<<<<<<<\e[0m"
 yum install nodejs -y
 
-echo ">>>>>>>>>>>>>>>>>>   creating application user   <<<<<<<<<<<<<<<<<"
+echo -e "\e[36m>>>>>>>>>>>>>>>>>>   creating application user   <<<<<<<<<<<<<<<<\e[0m"
 useradd roboshop
 
-echo ">>>>>>>>>>>>>>>>>>   create Application directory   <<<<<<<<<<<<<<<<<"
+echo -e "\e[36m>>>>>>>>>>>>>>>>>>   create Application directory   <<<<<<<<<<<<<<<<\e[0m"
 mkdir /app
 
-echo ">>>>>>>>>>>>>>>>>>   Download Application Content   <<<<<<<<<<<<<<<<<"
+echo -e "\e[36m>>>>>>>>>>>>>>>>>>   Download Application Content   <<<<<<<<<<<<<<<<\e[0m"
 curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip
 
-echo ">>>>>>>>>>>>>>>>>>   Extract Application content   <<<<<<<<<<<<<<<<<"
+echo -e "\e[36m>>>>>>>>>>>>>>>>>>   Extract Application content   <<<<<<<<<<<<<<<<\e[0m"
 cd /app
 unzip /tmp/catalogue.zip
 cd /app
 
-echo ">>>>>>>>>>>>>>>>>>   Download NodeJS Dependencies   <<<<<<<<<<<<<<<<<"
+echo -e "\e[36m>>>>>>>>>>>>>>>>>>   Download NodeJS Dependencies   <<<<<<<<<<<<<<<<\e[0m"
 npm install
 
-echo ">>>>>>>>>>>>>>>>>>   Install mongo client   <<<<<<<<<<<<<<<<<"
+echo -e "\e[36m>>>>>>>>>>>>>>>>>>   Install mongo client   <<<<<<<<<<<<<<<<\e[0m"
 yum install mongodb-org-shell -y
 
-echo ">>>>>>>>>>>>>>>>>>   Load catalogue schema   <<<<<<<<<<<<<<<<<"
+echo -e "\e[36m>>>>>>>>>>>>>>>>>>   Load catalogue schema   <<<<<<<<<<<<<<<<\e[0m"
 mongo --host MONGODB-SERVER-IPADDRESS </app/schema/catalogue.js
 
-echo ">>>>>>>>>>>>>>>>>>   start catalogue service   <<<<<<<<<<<<<<<<<"
+echo -e "\e[36m>>>>>>>>>>>>>>>>>>   start catalogue service   <<<<<<<<<<<<<<<<\e[0m"
 systemctl daemon-reload
 systemctl enable catalogue
 systemctl restart catalogue
