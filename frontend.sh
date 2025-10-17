bd0027a5c6f02e4d7701d3189d7a36a0b0436014
Bijay <bijay78082@gmail.com>
2025-10-17 14:03:03 +0530

    frontend

--- frontend.sh
+++ frontend.sh
@@ -1,35 +1,36 @@
-log=/tmp/roboshop.log
- echo -e "\e[33m<<<<<<<< install nginux >>>>>>>>>>>\e[0m"
-yum install nginx -y &>>${log}
-echo $?
+source common.sh
+
+ echo -e "\e[36m<<<<<<<< install nginx >>>>>>>>>>>\e[0m"
+yum install nginx -y
+func_exit_status
 
 
- echo -e "\e[33m<<<<<<< copy nginx-roboshop.conf file\e[0m"
+ echo -e "\e[36m<<<<<<< copy nginx-roboshop.conf file\e[0m"
 cp nginx-roboshop.conf /etc/nginx/default.d/roboshop.conf &>>${log}
-echo $?
+func_exit_status
 
- echo -e "\e[33m<<<<<<<<<<<<<<  remove all inside the /usr/share/nginx/html/* file >>>>>>>>>>>\e[0m"
+ echo -e "\e[36m<<<<<<<<<<<<<<  clean old content >>>>>>>>>>>\e[0m"
 rm -rf /usr/share/nginx/html/* &>>${log}
-echo $?
+func_exit_status
 
 
- echo -e "\e[33m <<<<<<<<<<<<<<<<<<<<    install unzip command   >>>>>>>>>>>>>>>>>>\e[0m"
+ echo -e "\e[36m <<<<<<<<<<<<<<<<<<<<    install unzip command   >>>>>>>>>>>>>>>>>>\e[0m"
 yum install unzip -y &>>${log}
-echo $?
+func_exit_status
 
- echo -e "\e[33m<<<<<<<<<<<<     download the zip file    >>>>>>>>>>>>>>>\e[0m"
+ echo -e "\e[33m<<<<<<<<<<<<     download Application content    >>>>>>>>>>>>>>>\e[0m"
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip &>>${log}
-echo $?
+func_exit_status
 
- echo -e "\e[33m<<<<<<<<<<<<<<<<<<<<   go to html directory     >>>>>>>>>>>>>>>\e[0m"
+ echo -e "\e[36m<<<<<<<<<<<<<<<<<<<<   go to html directory     >>>>>>>>>>>>>>>\e[0m"
 cd /usr/share/nginx/html &>>${log}
 echo $?
 
  echo -e "\e[33m <<<<<<<<<<<<<<<<<<<<<<     unzip frontend.zip file    >>>>>>>>>>>>>>>>>>>>>>>>>>\e[0m"
 unzip /tmp/frontend.zip &>>${log}
-echo $?
+func_exit_status
 
  echo -e "\e[33m <<<<<<<<<<<<<<<<<<<<<<<<     start and enable the nginx service     >>>>>>>>>>>>>>>>>>>>>>\e[0m"
 systemctl enable nginx &>>${log}
 systemctl restart nginx &>>${log}
-echo $?
+func_exit_status
